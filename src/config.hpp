c30ceff5be91ecad2d561ca3140f291ad178b6af
neville-n-d <nevillenataliodavisan@gmail.com>
2023-06-17 12:33:39 +0800

    change picture

--- src/config.hpp
+++ src/config.hpp
@@ -10,16 +10,15 @@
 #define timeout 10
 #define MAX_STEP 50
 
-
 /*Which character/words for pieces*/
 /* By default, the pieces are '♟', '♜', '♞', '♝', '♛', '♚' from unicode*/
-// #define PIECE_STR_LEN 1
-// const char piece_table[2][7][5] = {
-//   {" ", "♟", "♜", "♞", "♝", "♛", "♚"},
-//   {" ", "♙", "♖", "♘", "♗", "♕", "♔"},
-// };
-#define PIECE_STR_LEN 2
-const char PIECE_TABLE[2][7][5] = {
-  {"  ", "wP", "wR", "wn", "wB", "wQ", "wK"},
-  {"  ", "bP", "bR", "bn", "bB", "bQ", "bK"},
+#define PIECE_STR_LEN 1
+const char piece_table[2][7][5] = {
+    {" ", "♟", "♜", "♞", "♝", "♛", "♚"},
+    {" ", "♙", "♖", "♘", "♗", "♕", "♔"},
 };
+// #define PIECE_STR_LEN 2
+// const char PIECE_TABLE[2][7][5] = {
+//   {"  ", "wP", "wR", "wn", "wB", "wQ", "wK"},
+//   {"  ", "bP", "bR", "bn", "bB", "bQ", "bK"},
+// };
